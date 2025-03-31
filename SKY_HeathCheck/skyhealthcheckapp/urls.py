525a0b79c076cb708d0c222ae4b7bf1e724d0a8d
JodelDjan <w1973296@westminster.ac.uk>
2025-03-31 11:51:23 +0100

    Engineer and management login functions and url paths created

---
+++ SKY_HeathCheck/skyhealthcheckapp/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path
+from skyhealthcheckapp import views as managerLogin
+from skyhealthcheckapp import views as engineerLogin
+
+urlpatterns = [
+ path('admin/', admin.site.urls),
+ path('managerlogin/',managerLogin.managementLogin),
+ path('engineerlogin/',engineerLogin.managementLogin),
+ 
+]
