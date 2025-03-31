998674d7fc48d1649d74d0d069ef1c06d8e63129
JodelDjan <w1973296@westminster.ac.uk>
2025-03-31 12:00:37 +0100

    views.py additions

--- SKY_HeathCheck/skyhealthcheckapp/views.py
+++ SKY_HeathCheck/skyhealthcheckapp/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+#Management and Engineer login functions
+
+def managementLogin(request):
+    return HttpResponse('<h1>Log In</h1>')
+
+def engineerLogin(request):
+    return HttpResponse('<h1>Log In</h1>')
