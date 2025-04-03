152bedaefaf8d5259279c4dfc13c039b2f23330e
FoxParadeAFK <s.kittwong004@gmail.com>
2025-04-03 16:51:57 +0100

    Semi-working sign-up form

--- SKY_HeathCheck/Company/forms.py
+++ SKY_HeathCheck/Company/forms.py
@@ -1,14 +1,9 @@
+from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-
 
 class CreateUserForm(UserCreationForm):
   
   class Meta:
     model = User 
-    fields = ['username', 'first_name']
-    
-  def __init__(self, *args, **kwargs):
-    super().__init__(*args, **kwargs)
-    
-    # self.fields['username'].help_text = '<small> Hello </small>'
+    fields = ['username', 'first_name', 'last_name', 'email']
--- SKY_HeathCheck/Company/views.py
+++ SKY_HeathCheck/Company/views.py
@@ -1,9 +1,35 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login
 
 from Company.forms import CreateUserForm
 
 # Create your views here.
 def HandleSignupForm(request):
   form = CreateUserForm()
+  if (request == 'POST'):
+    form = CreateUserForm(request.POST)
+    
+    if (form.is_valid()):
+      form.save()
+      username = form.cleaned_data['username']
+      first_name = form.cleaned_data['first_name']
+      last_name = form.cleaned_data['last_name']
+      email = form.cleaned_data['email']
+      password = form.cleaned_data['password1']
+      
+      
+      user = authenticate(username = username, 
+                          password = password, 
+                          email = email,
+                          first_name = first_name,
+                          last_name = last_name)
+      
+      print(user)
+      
+      login(request, user)
+      
+  else:
+    form = CreateUserForm()
+      
   
   return render(request, 'Company/SignupPage.html', {'form': form})
