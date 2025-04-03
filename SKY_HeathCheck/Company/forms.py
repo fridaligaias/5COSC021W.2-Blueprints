c3ba834ce3621a28c4dcfaf9770a2187307bed0d
FoxParadeAFK <s.kittwong004@gmail.com>
2025-04-03 18:02:21 +0100

    Grouping added with the permissions

--- SKY_HeathCheck/Company/forms.py
+++ SKY_HeathCheck/Company/forms.py
@@ -1,9 +1,11 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
 
 class CreateUserForm(UserCreationForm):
   
+  group = forms.ModelChoiceField(Group.objects.all(), required = True)
+  
   class Meta:
     model = User 
-    fields = ['username', 'first_name', 'last_name', 'email', 'groups']
+    fields = ['username', 'first_name', 'last_name', 'email']
--- SKY_HeathCheck/Company/views.py
+++ SKY_HeathCheck/Company/views.py
@@ -16,6 +16,7 @@ def HandleSignupForm(request):
       last_name = form.cleaned_data['last_name']
       email = form.cleaned_data['email']
       password = form.cleaned_data['password1']
+      group = form.cleaned_data['group']
       
       
       user = authenticate(username = username, 
@@ -24,6 +25,7 @@ def HandleSignupForm(request):
       user.first_name = first_name
       user.last_name = last_name
       user.email = email
+      user.groups.add(group)
       
       
       if (user != None):
