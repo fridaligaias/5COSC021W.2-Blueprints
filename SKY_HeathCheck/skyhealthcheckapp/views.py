ec858a9913fea34734eb23bedfe87b9b001c5199
JodelDjan <162587511+JodelDjan@users.noreply.github.com>
2025-04-26 21:44:20 +0100

    Update views.py

--- SKY_HeathCheck/skyhealthcheckapp/views.py
+++ SKY_HeathCheck/skyhealthcheckapp/views.py
@@ -3,10 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-#Management and Engineer login functions
 
-def managementLogin(request):
-    return render (request , 'management_login.html')
-
-def engineerLogin(request):
-    return render (request , 'engineer_login.html')
