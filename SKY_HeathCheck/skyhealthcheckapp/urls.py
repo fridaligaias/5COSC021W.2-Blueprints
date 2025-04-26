94f9f13305bf96655437b050299f2e16e15277ae
JodelDjan <162587511+JodelDjan@users.noreply.github.com>
2025-04-26 21:44:43 +0100

    Update urls.py

--- SKY_HeathCheck/skyhealthcheckapp/urls.py
+++ SKY_HeathCheck/skyhealthcheckapp/urls.py
@@ -3,9 +3,4 @@ from django.urls import path
 from skyhealthcheckapp import views as managerLogin
 from skyhealthcheckapp import views as engineerLogin
 
-urlpatterns = [
- path('admin/', admin.site.urls),
- path('managerlogin/',managerLogin.managementLogin),
- path('engineerlogin/',engineerLogin.engineerLogin),
- 
-]
+
