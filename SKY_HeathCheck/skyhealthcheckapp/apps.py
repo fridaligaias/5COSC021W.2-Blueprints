89cb18234bff42187ff5448ec624d7639b5ee042
JodelDjan <162587511+JodelDjan@users.noreply.github.com>
2025-04-26 21:36:59 +0100

    Update apps.py

--- SKY_HeathCheck/skyhealthcheckapp/apps.py
+++ SKY_HeathCheck/skyhealthcheckapp/apps.py
@@ -1,6 +1,11 @@
 from django.apps import AppConfig
+from django.contrib.auth.models import Group, User
 
-
-class SkyhealthcheckappConfig(AppConfig):
+class TestappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'skyhealthcheckapp'
+    name = 'testapp'
+
+def ready(self):
+        group_names = ['Admin', 'Senior Manager', 'Department Leader', 'Team Leader', 'Engineer']
+        for group_name in group_names:
+            Group.objects.get_or_create(name=group_name)
