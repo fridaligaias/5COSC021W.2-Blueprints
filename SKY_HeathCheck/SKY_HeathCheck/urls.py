7a8790bcf99acbb2f3bae0adb6c2cc15283c2061
JodelDjan <162587511+JodelDjan@users.noreply.github.com>
2025-04-26 21:38:46 +0100

    Update urls.py

--- SKY_HeathCheck/SKY_HeathCheck/urls.py
+++ SKY_HeathCheck/SKY_HeathCheck/urls.py
@@ -21,10 +21,11 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('django.contrib.auth.urls')),  # Includes login, logout, password management
+    path('accounts/', include('django.contrib.auth.urls')),  # login, logout, password management
     path('register/', views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
 
+    #dashboard enpoints
     path('dashboard/', views.dashboard, name='dashboard'),
     path('admin_dashboard/', views.admin_dashboard, name='admin_dashboard'),
     path('departmentleader_dashboard/', views.departmentLeader_dashboard, name='departmentleader_dashboard'),
